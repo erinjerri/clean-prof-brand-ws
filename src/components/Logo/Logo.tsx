fb05f140c2899f0bdc8f48162a8e3f784db26ea6
Erin Jerri Pangilinan <3184381+erinjerri@users.noreply.github.com>
2025-05-27 19:09:27 -0700

    fixed closed tag for  logo image

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -24,3 +24,6 @@ export const Logo = (props: Props) => {
       decoding="async"
       className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
       src="https://github.com/erinjerri/clean-prof-brand-ws/blob/main/public/logo-3De-blue-transparent.png"
+    />
+  )
+}
